0534f7d0ae08a78c613a768c2d30780bc84b4a74
Remington.Greenbauer <remingt.greenbauer@blackbaud.com>
2022-01-27 10:34:43 -0500

    Bones of Express and React app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [testApiData, setTestApiData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const testFetchCall = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json")
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders
+    };
+
+    return fetch(`http://localhost:8080`, requestOptions)
+  }
+
+  useEffect(() => {
+    testFetchCall()
+      .then(res => res.json())
+      .then(data => { console.log(data.express); setTestApiData(data.express); setIsLoading(false) });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {isLoading ? 'Still Loading...' : testApiData}
     </div>
   );
 }
