dacc6d257bfd23e1771de05a92b2290fb088ddd5
Remington.Greenbauer <remington.greenbauer@blackbaud.com>
2022-02-13 19:50:39 -0500

    test new email

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,33 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
 const Header = (props) => {
   
+  const [photoUrl, setPhotoUrl] = useState("");
   const navigate = useNavigate();
 
   const navigateToHome = () => {
     navigate("/");
   }
 
+  const profilePicture = (
+    <img onClick={handleModal} />
+  )
+
+  const defaultProfilePicture = (
+    <img onClick={handleModal} />
+    );
+
   const handleModal = () =>
     props.updateModalDisplay(props.modalStatus ? false : true);
+
 
   return (
     <nav className={styles.headerContainer}>
       <svg onClick={navigateToHome}></svg>
-      <img onClick={handleModal} />
+      {photoUrl == "" ? defaultProfilePicture : profilePicture}
     </nav>
   );
 };
